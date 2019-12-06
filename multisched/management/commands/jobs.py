4395c32125a054f71b6f5fbcfc392a5ea219ddda
Kanishk Singh <singh.kanishk07@gmail.com>
2019-12-06 19:19:25 +0530

    Add rqscheduler management command

---
+++ multisched/management/commands/jobs.py
@@ -0,0 +1,34 @@
+import logging
+
+import django_rq
+from django_rq.management.commands import rqscheduler
+
+from multisched.tasks import say_hello
+
+scheduler = django_rq.get_scheduler()
+log = logging.getLogger(__name__)
+
+
+def clear_scheduled_jobs():
+    # Delete any existing jobs in the scheduler when the app starts up
+    for job in scheduler.get_jobs():
+        log.debug("Deleting scheduled job %s", job)
+        job.delete()
+
+
+def register_scheduled_jobs():
+    # do your scheduling here
+    scheduler.cron(
+        "* * * * *",    # Run every minute
+        func=say_hello, # Function to be queued
+        repeat=None     # Repeat this number of times (None means repeat forever)
+    )
+
+
+class Command(rqscheduler.Command):
+    """Simple test scheduling job"""
+    def handle(self, *args, **kwargs):
+        # This is necessary to prevent dupes
+        clear_scheduled_jobs()
+        register_scheduled_jobs()
+        super(Command, self).handle(*args, **kwargs)
